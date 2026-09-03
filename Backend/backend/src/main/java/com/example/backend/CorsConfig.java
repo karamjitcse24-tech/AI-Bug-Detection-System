8f87f64aa5494a4cf890247b31dba3be4b2fe7f2
karamjitcse24-tech <karamjit.cse24@satyug.edu.in>
2026-09-03 22:09:30 +0530

    Update CorsConfig.java

--- Backend/backend/src/main/java/com/example/backend/CorsConfig.java
+++ Backend/backend/src/main/java/com/example/backend/CorsConfig.java
@@ -14,7 +14,6 @@ public class CorsConfig {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
