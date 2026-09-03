2a9e39441c44379c921ba5af07863fdc6b1fa6b0
karamjitcse24-tech <karamjit.cse24@satyug.edu.in>
2026-09-03 21:03:26 +0530

    Update bugService.js

--- Frontend/src/services/bugService.js
+++ Frontend/src/services/bugService.js
@@ -1,25 +1,20 @@
 import axios from "axios";
 
-// Railway Backend URL
 const API_URL =
   "https://ai-bug-detection-system-production.up.railway.app/bugs";
 
-// Get all bugs
 export const getAllBugs = () => {
   return axios.get(API_URL);
 };
 
-// Create a new bug
 export const createBug = (bug) => {
   return axios.post(API_URL, bug);
 };
 
-// Update a bug
 export const updateBug = (id, bug) => {
   return axios.put(`${API_URL}/${id}`, bug);
 };
 
-// Delete a bug
 export const deleteBug = (id) => {
   return axios.delete(`${API_URL}/${id}`);
 };
